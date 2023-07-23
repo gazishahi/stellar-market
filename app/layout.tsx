cd260f7040ba141ba3a7c390308f30c73f3c8205
ArnanK <arnank9@gmail.com>
2023-07-23 16:21:54 -0400

    auth0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Roboto, Poppins } from 'next/font/google'
 import Navbar from './components/navbar'
 import Subnav from './components/subnav'
 import './components/styles/globals.css'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({
@@ -26,9 +27,15 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <Navbar/>
+      <UserProvider>
+        <Navbar/>
+      </UserProvider>
       <Subnav/>
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <UserProvider>  
+          {children}
+        </UserProvider>
+      </body>
     </>
       
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
+'use client'
 import Navbar from "./components/navbar"
 import { prisma } from "@/prisma/db"
+import { useUser } from "@auth0/nextjs-auth0/client"
 
 function getUsers(){
   return prisma.user.findMany()
 }
 
-
 export default async function Home() {
-  const users = await getUsers()
+  //const users = await getUsers()
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   return (
-    <>
-    <main>
-      {users.map(user=>(
-        <li>{user.email}</li>
-      ))}
-    </main>
-    </>
-  )
+    user && (
+      <div>
+        <h2>{user.name}</h2>
+      </div>
+    )
+  );
 }
