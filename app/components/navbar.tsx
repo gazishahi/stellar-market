b4239b49352a2d10c2f192f71c600149404618b1
ArnanK <arnank9@gmail.com>
2023-07-25 10:48:47 -0400

    merge ready

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,24 +1,21 @@
 'use client';
+
 import Link from 'next/link';
 import {useState} from 'react';
 
 const Navbar = () => {
     const [searchQuery, setSearchQuery] = useState("");
     return (
-        <> 
-        
-            <div className='pt-5'>
-                <nav className='header'>
-                    <Link href="/">StellarMarket</Link>
-                    <input onChange={(event) => setSearchQuery(event.target.value)} type='text' placeholder='Search StellarMarket' className='px-5 py-3 w-1/3 sm:px-5 sm:py-3 text-zinc-200 bg-zinc-800 focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-blue-100 placeholder:text-zinc-400 text-center'></input>
-                    <ul>
-                    <Link href="/cart">Cart</Link>
-                    <Link href="/login">Log in</Link>   
-                    </ul>
-                </nav>
-            </div>
-        
-        </>
+        <div className='pt-5'>
+            <nav className='header'>
+                <Link href="/" className='font-bold text-4xl'>Stellar<span className="text-blue-600">Market</span></Link>
+                <input onChange={(event) => setSearchQuery(event.target.value)} type='text' placeholder='Search StellarMarket' className='px-5 py-3 w-1/3 sm:px-5 sm:py-3 text-zinc-200 bg-zinc-800 focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-blue-100 placeholder:text-zinc-400 text-center'></input>
+                <ul>
+                <Link href="/cart">Cart</Link>
+                <Link href="/login">Log In</Link>   
+                </ul>
+            </nav>
+        </div>
     )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,57 @@
+import Image from "next/image"
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 import User from './components/user'
 
-export default async function Home() {
-  const session = await getServerSession(authOptions)
 
+export default function Home() {
   return (
-    <section>
-      <h1>Home</h1>
-      <h1>Server Side Rendered</h1>
-      <pre>{JSON.stringify(session)}</pre>
-      <h1>Client Side Rendered</h1>
-      <User />
-    </section>
+    <>
+    <main>
+      <section className="lg:grid lg:grid-cols-6 lg:grid-rows-2">
+        <div className="lg:col-span-4 lg:row-span-2 m-3">
+          <a className="block h-full">
+            <div className="flex h-full w-full items-center justify-center overflow-hidden bg-sky-400 dark:bg-sky-600 relative rounded-3xl">
+            <Image src="/White-Sneakers-PNG-Clipart.png" height={500} width={500} alt="Sneakers" className="relative h-full w-full object-contain transition duration-300 ease-in-out hover:scale-105"/>
+            <div className="absolute left-0 top-0 w-3/4 text-black dark:text-white">
+              <h3 className="inline bg-white box-decoration-clone py-3 pl-5 font-semibold leading-loose shadow-[1.25rem_0_0] shadow-white dark:bg-black dark:shadow-black text-3xl rounded-br-3xl">Sneakers</h3>
+              <p className="w-fit bg-white px-5 py-3 text-sm font-semibold dark:bg-black dark:text-white rounded-br-3xl">$70.00</p>
+            </div>
+            </div>
+          </a>
+        </div>
+        <div className="lg:col-span-2 lg:row-span-1 m-3">
+          <a className="block h-full">
+            <div className="flex h-full w-full items-center justify-center overflow-hidden bg-gray-900 dark:bg-gray-900 relative rounded-3xl">
+              <div className="absolute left-0 top-0 w-3/4 text-black dark:text-white">
+                <h3 className="inline bg-white box-decoration-clone py-3 pl-5 font-semibold leading-loose shadow-[1.25rem_0_0] shadow-white dark:bg-black dark:shadow-black text-3xl rounded-br-3xl">Something</h3>
+                <p className="w-fit bg-white px-5 py-3 text-sm font-semibold dark:bg-black dark:text-white rounded-br-3xl">$15.00</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="lg:col-span-2 lg:row-span-1 m-3">
+          <a className="block h-full">
+          <div className="flex h-full w-full items-center justify-center overflow-hidden bg-gray-900 dark:bg-gray-900 relative rounded-3xl">
+              <div className="absolute left-0 top-0 w-3/4 text-black dark:text-white">
+                <h3 className="inline bg-white box-decoration-clone py-3 pl-5 font-semibold leading-loose shadow-[1.25rem_0_0] shadow-white dark:bg-black dark:shadow-black text-3xl rounded-br-3xl">Another thing</h3>
+                <p className="w-fit bg-white px-5 py-3 text-sm font-semibold dark:bg-black dark:text-white rounded-br-3xl">$23.00</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </section>
+      <div className="relative w-full overflow-hidden bg-black dark:bg-white rounded-3xl">
+        <div className="flex animate-[wiggle_1s_ease-in-out_infinite]">
+          <a className="m-3">1</a>
+          <a className="m-3">2</a>
+          <a className="m-3">3</a>
+          <a className="m-3">4</a>
+          <a className="m-3">5</a>
+          <a className="m-3">6</a>
+        </div>
+      </div>
+    </main>
+    </>
   )
 }
