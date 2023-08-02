949b77959a6e8ad90070e4032822a3ee4200d86f
ArnanK <arnank9@gmail.com>
2023-08-02 15:06:29 -0400

    added users table to migrations.sql

--- prisma/migrations/20230802190322_init/migration.sql
+++ prisma/migrations/20230802190322_init/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE "User" (
     "id" STRING NOT NULL,
     "email" STRING NOT NULL,
     "name" STRING,
-    "password" STRING NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -19,6 +18,18 @@ CREATE TABLE "Product" (
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "users"(
+    "id" UUID DEFAULT uuid_generate_v4(),
+    "nickname" VARCHAR(255),
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password" VARCHAR(255) NOT NULL,
+    "email_Verified" BOOLEAN DEFAULT FALSE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
