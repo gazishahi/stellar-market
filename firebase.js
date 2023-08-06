a2df5d09fff9744589f7f46a4ac8398a928fcf1b
ArnanK <arnank9@gmail.com>
2023-08-05 20:04:50 -0400

    security

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCCOMOeO733LmgMR0-sA1oDt-95ajcu7nY",
-    authDomain: "next-stellarmarket.firebaseapp.com",
-    projectId: "next-stellarmarket",
-    storageBucket: "next-stellarmarket.appspot.com",
-    messagingSenderId: "931006656140",
-    appId: "1:931006656140:web:6330496a74519b1efb95a9"
+    //enter key info
 }
 
 const app = initializeApp(firebaseConfig)
