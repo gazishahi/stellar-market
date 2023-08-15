f28a49d24e4aff186ceaa7e1e7c82f22523486f9
ArnanK <arnank9@gmail.com>
2023-08-15 00:07:20 -0400

    Display products to homepage

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCCOMOeO733LmgMR0-sA1oDt-95ajcu7nY",
-    authDomain: "next-stellarmarket.firebaseapp.com",
-    projectId: "next-stellarmarket",
-    storageBucket: "next-stellarmarket.appspot.com",
-    messagingSenderId: "931006656140",
-    appId: "1:931006656140:web:6330496a74519b1efb95a9"
+    apiKey: process.env.NEXT_PUBLIC_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_APPID
     
 }
 
